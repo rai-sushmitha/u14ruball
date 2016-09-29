e8983db2df57ed1881b08eabeefcfac564faa7c8
deepikasl <deepika@shippable.com>
2016-09-29 17:02:01 +0530

    download ree_1.8.7 from a correct url

--- version/ree_1_9_3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "================= Installing REE 1.9.3 ==================="
-rvm requirements
-rvm install ree-1.9.3
-rvm use ree-1.9.3
-
-# Install ruby gems
-gem install bundler
