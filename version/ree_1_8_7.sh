eb71590eef6baabb3b8a0acca184cb90c885575c
mrinalam <mrinal.cena@gmail.com>
2017-02-07 12:04:54 +0000

    changes made to ree_1_8_7.sh

--- version/ree_1_8_7.sh
+++ version/ree_1_8_7.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 
-echo "================= Installing REE 1.8.7 ==================="
-apt-get install -y libreadline-dev zlib1g-dev libssl-dev libxslt-dev libxml2-dev
-cd /usr/src
-wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rubyenterpriseedition/ruby-enterprise-1.8.7-2012.02.tar.gz
-tar xzf ruby-enterprise-1.8.7-2012.02.tar.gz && cd ruby-enterprise-1.8.7-2012.02
-cd ./source
+echo "================= Installing Ruby 2.1.3 ==================="
+rvm requirements
+rvm install ree-1.8.7
+rvm use ree-1.8.7
 
 # Install ruby gems
 gem install bundler
