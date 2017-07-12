bd4809018d9748f70cde2d8ea1467a6aa965d6cd
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-12 16:40:13 +0530

    updating versions of ruby

--- version/ree_1_8_7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo "================= Installing Ruby 2.1.3 ==================="
-rvm requirements
-rvm install ree-1.8.7
-rvm use ree-1.8.7
-
-# Install ruby gems
-gem install bundler
