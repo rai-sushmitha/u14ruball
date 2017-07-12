3871992e3f729964648062861a9d6c883beb3458
rai-sushmitha <sushmitharai210@gmail.com>
2017-07-12 16:40:13 +0530

    updating versions of ruby

--- test/_ruby.sh
+++ test/_ruby.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
 source /usr/local/rvm/scripts/rvm
 
-echo "rvm ls"
-rvm ls
-printf "\n"
 
-echo "rvm use jruby-1.7.19"
+echo "rvm use jruby-9.1.12"
 rvm use jruby-1.7.19
 printf "\n"
 
@@ -13,54 +10,19 @@ echo "rvm use jruby-9.0.0"
 rvm use jruby-9.0.0
 printf "\n"
 
-echo "rvm use ree-1.8.7"
-rvm use ree-1.8.7
+echo "rvm use jruby-1.7.27"
+rvm use jruby-1.7.27
 printf "\n"
 
-echo "rvm use 1.8.7"
-rvm use 1.8.7
+
+echo "rvm use 2.2.7"
+rvm use 2.2.7
 printf "\n"
 
-echo "rvm use 1.9.2"
-rvm use 1.9.2
+echo "rvm use 2.3.4"
+rvm use 2.3.4
 printf "\n"
 
-echo "rvm use 1.9.3"
-rvm use 1.9.3
-printf "\n"
-
-echo "rvm use 2.0.0"
-rvm use 2.0.0
-printf "\n"
-
-echo "rvm use 2.1.2"
-rvm use 2.1.2
-printf "\n"
-
-echo "rvm use 2.1.3"
-rvm use 2.1.3
-printf "\n"
-
-echo "rvm use 2.1.4"
-rvm use 2.1.4
-printf "\n"
-
-echo "rvm use 2.1.5"
-rvm use 2.1.5
-printf "\n"
-
-echo "rvm use 2.2.0"
-rvm use 2.2.0
-printf "\n"
-
-echo "rvm use 2.2.1"
-rvm use 2.2.1
-printf "\n"
-
-echo "rvm use 2.3.2"
-rvm use 2.3.2
-printf "\n"
-
-echo "rvm use 2.3.3"
-rvm use 2.3.3
+echo "rvm use 2.4.1"
+rvm use 2.4.1
 printf "\n" 
